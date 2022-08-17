7fb9f822ebd67ecb0cc06a19ea5653dfc0b17b80
Mukundkh <khandelwalmukund23@gmail.com>
2022-08-17 16:08:14 +0530

    move All negatives

--- src/Arrays/MoveAllNegatives.java
+++ src/Arrays/MoveAllNegatives.java
@@ -2,7 +2,6 @@ package Arrays;
 
 public class MoveAllNegatives {
 
-
     public static void print(int input[]){
 
         for(int i=0; i<input.length; i++){
@@ -10,24 +9,38 @@ public class MoveAllNegatives {
         }
     }
 
-    public static void moveAllNegatives(int input[]) {
+    public static void moveAllNegatives(int input[]){
 
-        int count = 0;
-        int negCount = 0;
-        for (int i = 0; i < input.length; i++) {
-            if (input[i] < 0) {
-                input[count] = input[i];
-                count++;
+        int left = 0;
+        int right = input.length-1;
 
+        while(left <= right){
+
+            if(input[left] > 0 && input[right] > 0){
+                right--;
             }
-        }
+            else if(input[left] < 0 &&  input[right] < 0){
+                left++;
+            }
+            else if(input[left] > 0 && input[right] < 0){
+                int temp = input[left];
+                input[left] = input[right];
+                input[right] = temp;
 
+                left++;
+                right--;
+
+            }else{
+                left++;
+            }
+
+        }
     }
 
     public static void main(String[] args) {
-        int input[] = {1, 2, -1, 3, -2, 5, -100};
+        int input[] = {-1, 2, 10, -2, 2, 19, -100};
         moveAllNegatives(input);
         print(input);
-
     }
+
 }
