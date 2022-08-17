64bc5cde052197ca1c39cb43a0b8cc7ef14dd8d2
Mukundkh <khandelwalmukund23@gmail.com>
2022-08-17 17:51:06 +0530

    intersection of two arrays

--- src/Arrays/UnionAndIntersection.java
+++ src/Arrays/UnionAndIntersection.java
@@ -31,11 +31,37 @@ public class UnionAndIntersection {
         }
     }
 
+    public static void intersectionOfArrays(int arr1[], int arr2[]){
+
+        //Intersection of two arrays
+        //Firstly created a freq hashmap for first array
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        for(int i=0; i<arr1.length; i++){
+            int val = arr1[i];
+            if(map.containsKey(val)){
+                int of = map.get(val);
+                int nf = of + 1;
+                map.put(val, nf);
+            }else{
+                map.put(val, 1);
+            }
+        }
+        //Checking the key values of created hashmap with the elements present in second array
+        for(int i=0; i<arr2.length; i++){
+            int val = arr2[i];
+            if(map.containsKey(val)){
+                System.out.print(val + " ");
+            }
+        }
+    }
+
     public static void main(String[] args) {
 
-        int a[] = {0, 1, 1, 3, 3};
-        int b[] = {2,1,2};
-        unionOfArrays(a, b);
+        int a[] = {1, 2, 3};
+        int b[] = {1, 4, 5, 6, 7, 3};
+        //unionOfArrays(a, b);
+        intersectionOfArrays(a, b);
     }
 
 }
